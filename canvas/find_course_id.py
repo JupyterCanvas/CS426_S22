7215499aa3e5d54e8208bc06b05c8066f8bf8980
Sarah Skidmore <obeytheviszla@gmail.com>
2022-03-03 23:44:09 -0800

    Removed hardcoding, takes search string as command line arg. Provides CLI error messages and help

--- canvas/find_course_id.py
+++ canvas/find_course_id.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 
+# Returns list of course ids from provided search term
+# ./find_course_id search term
+
 import json # requests to Canvas api return json objects
 import re # for regular expression matching (in pagination)
-import sys # for sys.exit()
+import sys # for sys.exit, sys.argv
 import pprint # pretty print python data structures
 import logging 
 import requests # to send HTTP requests with Python
 import argparse # for adding CLI tags/help
+import textwrap # for formatting argparse help
 
-# can setup CLI flags and help here
+# setup CLI args and help
 parser = argparse.ArgumentParser(
-        description='',
-        epilog='\
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        Find Canvas course id with search term:
+          the partial course name, subject or number.
+          Must be at least 3 characters.
+        '''),
+        epilog='Copy the correct course id # from the results \
         ')
-##parser.add_argument('searchterm', type=str, help='the string to search for')
-##args = parser.parse_args()
+
+parser.add_argument('searchterm', type=str, nargs='+',
+                    help='the string to search for \
+                    such as "CS 426" or "senior projects"')
+args = parser.parse_args()
 
 # setup logging:
 LOG_FILE= ''
@@ -32,10 +44,7 @@ logger.addHandler(logger_fh)
 # on your canvas page, go to Account, Settings, Approved Integrations to
 # generate an access token. 
 
-# student employee as teacher cannot add/remove or retrieve some student info,
-# can get some info about course like section id
-
-# get Zach to create an expiring token, put in local_settings.py file as: 
+# put in local_settings.py file as: 
 # ACCESS_TOKEN="your access token string"
 # (we don't want access tokens in repository code!)
 try:
@@ -43,8 +52,10 @@ try:
 except ImportError:
     pass
 
+
 BASE_URL = "https://canvas.instructure.com" 
 ACCOUNT_ID = "44240000000000034" # College of Engineering 
+
 
 # find course id with search term: The partial course name, code, or full ID to
 # match and return in the results list. Must be at least 3 characters.
@@ -83,15 +94,18 @@ def get_course_id(base_url, account_id, search_term, headers):
 
 
 def main():
+    #search_term = args.searchterm
+    search_term = ' '.join(sys.argv[1:])
+    
     headers = {'Authorization': 'Bearer {access_token}'.format(access_token=ACCESS_TOKEN)}
     #logging.info(headers)
 
     # find course id:
-    #courses = get_course_id(BASE_URL, ACCOUNT_ID, "Jupyter", headers)
-    courses = get_course_id(BASE_URL, ACCOUNT_ID, "CS 135", headers)
+    #courses = get_course_id(BASE_URL, ACCOUNT_ID, "cs 135", headers)
+    courses = get_course_id(BASE_URL, ACCOUNT_ID, search_term, headers)
     #pprint.pprint(courses)
     for c in range(len(courses)):
-        print(f"\t{c} : COURSE_ID : {courses[c]['id']} : {courses[c]['name']}\n")
+        print(f"\t{c}. COURSE_ID : {courses[c]['id']} : {courses[c]['name']}\n")
     sys.exit(0)
 
 if __name__ == "__main__":
