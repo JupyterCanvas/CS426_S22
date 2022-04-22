6b06b8e565563509fb987226eeb073e2658999a0
Sarah Skidmore <obeytheviszla@gmail.com>
2022-04-22 11:08:49 -0700

    temp stash, need to increase disk size of VM, will be updating comments/readmes and recommit chages to main dev branch for these changes

--- canvas/cleanup/05-remove_containers.py
+++ canvas/cleanup/05-remove_containers.py
@@ -66,5 +66,21 @@ def main():
     else:
         logger.info("Error removing leftover vncservers")
 
+    # a Windows update (and unexpected shutdown of VM) revealed a bug: 
+    # leftover cni config files may prevent containers from being generated
+    # (journalctl -xe was reporting error: INFO:    Cleaning up image... ERROR:   container cleanup failed: no instance found with name inst-cs123-newellz2)
+    # (cont@.service status was reporting reporting error: requested IP address 10.0.123.10 is not available in range set 10.0.123.1-10.0.123.254)
+    # root@jupytercanvas:/var/lib/cni/networks# ls fakeroot/
+    # 10.0.123.10  10.0.123.11  10.0.123.12  10.0.123.13  10.0.123.14  last_reserved_ip.0  lock
+    # rm -r fakeroot # removes locks, containers can now be created with canvas/create-containers.py
+
+    # remove ip locks: 
+    p = subprocess.run(["rm", "-r", "/var/lib/cni/networks/*"], stdout=PIPE)
+    if p.returncode == 0: 
+        logger.info("IP locks removed")
+    else: 
+        logger.info("Error removing container IP locks")
+
+
 if __name__ == "__main__":
     main()
