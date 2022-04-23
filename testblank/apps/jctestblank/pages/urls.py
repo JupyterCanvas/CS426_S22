b7a314553216beffe49e76548116f1650fabcfe5
Sarah Skidmore <obeytheviszla@gmail.com>
2022-04-23 09:48:24 -0700

    temp commit/archive django test apps

---
+++ testblank/apps/jctestblank/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import HomePageView, AboutPageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', include('django.contrib.auth.urls')),
+]
