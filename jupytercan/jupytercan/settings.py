e2c48b85c07513fca4b32be4122d269319430e03
Sarah Skidmore <obeytheviszla@gmail.com>
2022-04-28 15:59:09 -0700

    updated server name in settings allowed hosts

--- jupytercan/jupytercan/settings.py
+++ jupytercan/jupytercan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '172.20.195.248']
+ALLOWED_HOSTS = ['localhost', '172.20.195.248', 'jupyter-canvas.cse.unr.edu']
 
 
 # Application definition
